4922d4fda72dfe990e6c7aa663ce4d57a6e6633f
Logan Lindquist <logan@legacybeta.com>
2015-06-18 10:52:27 -0500

    Initial Commit

---
+++ coffee timerTests/coffee_timerTests.swift
@@ -0,0 +1,36 @@
+//
+//  coffee_timerTests.swift
+//  coffee timerTests
+//
+//  Created by Logan Lindquist on 6/18/15.
+//  Copyright (c) 2015 Logan Lindquist. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class coffee_timerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
