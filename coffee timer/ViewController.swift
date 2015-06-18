e73d3db038887862b2a479d15b591a84219af82d
Logan Lindquist <logan@legacybeta.com>
2015-06-18 13:45:41 -0500

    annnd we have a button

--- coffee timer/ViewController.swift
+++ coffee timer/ViewController.swift
@@ -12,12 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("view is loaded. ")
+        view.backgroundColor = .orangeColor()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+
+    @IBAction func buttonWasPressed(sender: AnyObject) {
+        println("button was pressed")
     }
 
 
